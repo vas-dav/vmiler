2b5b7112991c770bf27bab83c6ffc0580a5bcd4a
vas-dav <vasilydavydovmail@yahoo.com>
2023-11-29 22:46:52 +0200

    vmi: init cpp rewrite

---
+++ vmi.cc
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  vmi.cpp
+ *
+ *    Description: A main source file of vmiler project 
+ *
+ *        Version:  1.0
+ *        Created:  29.11.2023 22.40.02
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  tylen, mishanya
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+
+int main (int argc, char** argv) {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
+
